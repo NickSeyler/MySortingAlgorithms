175d5e48de84091b9189adca76aa3088724f63ad
Nick Seyler <49881154+NickSeyler@users.noreply.github.com>
2021-09-30 14:16:58 -0400

    Add files via upload

---
+++ CPP/SortingAlgorithms.h
@@ -0,0 +1,18 @@
+//Author: Nick Seyler
+//Date: 9/29/2020
+//File: SortingAlgorithms.h
+
+#pragma once
+
+class SortingAlgorithms
+{
+public:
+	void bubbleSort(int arr[], int len);
+	void insertionSort(int arr[], int len);
+	void selectionSort(int arr[], int len);
+	void mergeSort(int arr[], int len);
+	void quickSort(int arr[], int len);
+
+	void swap(int* x, int* y);
+	void printArr(int arr[], int len);
+};
